417e4450136002264aca10b79ebb700e826fc2d9
Martin <240703@vutbr.cz>
2023-04-05 21:27:40 +0200

    Some changes on bin_morse

--- ProjectMorse/ProjectMorse.srcs/sources_1/new/bin_morse.vhd
+++ ProjectMorse/ProjectMorse.srcs/sources_1/new/bin_morse.vhd
@@ -1,5 +1,6 @@
 library ieee;
   use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 ------------------------------------------------------------
 -- Entity declaration for seven7-segment display decoder
@@ -54,128 +55,128 @@ begin
     end if;
     
     if (rising_edge(clk)) and (send_en = '1') then
+      if (sig_en = '1') then
+        case bin is
         
-      case bin is
+            when "00001" => --A
+                if (sig_cnt < c_DELAY_dot) then
+                  sig_cnt <= sig_cnt + 1;
+                else
+                  -- Move to the next state
+                  sig_state <= WEST_GO;
+                  -- Reset delay counter value
+                  sig_cnt   <= (others => '0');
+                end if;       
+          
+            when "00010" =>
     
-        when "00001" => -- A
-
-            if (sig_cnt < c_DELAY_2SEC) then
-              sig_cnt <= sig_cnt + 1;
-            else
-              -- Move to the next state
-              sig_state <= WEST_GO;
-              -- Reset delay counter value
-              sig_cnt   <= (others => '0');
-            end if;       
-      
-        when "00010" =>
-
-          seg <= "1100000"; -- B
-
-        when "00011" =>
-
-          seg <= "0110001"; -- C     
-          
-        when "00100" =>
-
-          seg <= "1000010"; -- D             
-
-        when "00101" =>
-
-          seg <= "0110000"; -- E
-          
-        when "00110" =>
-
-          seg <= "0111000"; -- F
-        
-        when "00111" =>
-
-          seg <= "0100001"; -- G
-        
-        when "01000" =>
-
-          seg <= "1101000"; -- H
-        
-        when "01001" =>
-
-          seg <= "0111011"; -- I
-        
-        when "01010" =>
-
-          seg <= "0100111"; -- J
-        
-        when "01011" =>
-
-          seg <= "0101000"; -- K
-        
-        when "01100" =>
-
-          seg <= "1110001"; -- L
-        
-        when "01101" =>
-
-          seg <= "0101010"; -- M
-        
-        when "01110" =>
-
-          seg <= "1101010"; -- N
-        
-        when "01111" =>
-
-          seg <= "1100010"; -- O
-        
-        when "10000" =>
-
-          seg <= "0011000"; -- P
-        
-        when "10001" =>
-
-          seg <= "0001100"; -- Q
-        
-        when "10010" =>
-
-          seg <= "1111010"; -- R
-        
-        when "10011" =>
-
-          seg <= "0100101"; -- S
-        
-        when "10100" =>
-
-          seg <= "1110000"; -- T
-        
-        when "10101" =>
-
-          seg <= "1100011"; -- U
-        
-        when "10110" =>
-
-          seg <= "1010101"; -- V
-        
-        when "10111" =>
-
-          seg <= "1010100"; -- W
-        
-        when "11000" =>
-
-          seg <= "1101011"; -- X
-        
-        when "11001" =>
-
-          seg <= "1000100"; -- Y
-        
-        when "11010" =>
-
-          seg <= "0010011"; -- Z       
-
-        when others =>
-
-          seg <= "1111111"; -- nothing
-
-      end case;
-
+              seg <= "1100000"; -- B
+    
+            when "00011" =>
+    
+              seg <= "0110001"; -- C     
+              
+            when "00100" =>
+    
+              seg <= "1000010"; -- D             
+    
+            when "00101" =>
+    
+              seg <= "0110000"; -- E
+              
+            when "00110" =>
+    
+              seg <= "0111000"; -- F
+            
+            when "00111" =>
+    
+              seg <= "0100001"; -- G
+            
+            when "01000" =>
+    
+              seg <= "1101000"; -- H
+            
+            when "01001" =>
+    
+              seg <= "0111011"; -- I
+            
+            when "01010" =>
+    
+              seg <= "0100111"; -- J
+            
+            when "01011" =>
+    
+              seg <= "0101000"; -- K
+            
+            when "01100" =>
+    
+              seg <= "1110001"; -- L
+            
+            when "01101" =>
+    
+              seg <= "0101010"; -- M
+            
+            when "01110" =>
+    
+              seg <= "1101010"; -- N
+            
+            when "01111" =>
+    
+              seg <= "1100010"; -- O
+            
+            when "10000" =>
+    
+              seg <= "0011000"; -- P
+            
+            when "10001" =>
+    
+              seg <= "0001100"; -- Q
+            
+            when "10010" =>
+    
+              seg <= "1111010"; -- R
+            
+            when "10011" =>
+    
+              seg <= "0100101"; -- S
+            
+            when "10100" =>
+    
+              seg <= "1110000"; -- T
+            
+            when "10101" =>
+    
+              seg <= "1100011"; -- U
+            
+            when "10110" =>
+    
+              seg <= "1010101"; -- V
+            
+            when "10111" =>
+    
+              seg <= "1010100"; -- W
+            
+            when "11000" =>
+    
+              seg <= "1101011"; -- X
+            
+            when "11001" =>
+    
+              seg <= "1000100"; -- Y
+            
+            when "11010" =>
+    
+              seg <= "0010011"; -- Z       
+    
+            when others =>
+    
+              seg <= "1111111"; -- nothing
+    
+          end case;
+    
+    send_en <= '0';
     end if;
 
-  end process p_7seg_decoder;
+  end process p_bin_morse_decoder;
 
 end architecture behavioral;
